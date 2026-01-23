3263b6058cff101870d618de07090e64703f9623
Opilane TTHK <opilane@tthk.ee>
2026-01-23 11:41:47 +0200

    DOWHILE ülesanne 2 koos Vooskeemiga

--- TSÜKLID/DOWHILE/Program.cs
+++ TSÜKLID/DOWHILE/Program.cs
@@ -14,6 +14,7 @@
             // -> do while tsükli tingimuseks kirjuta kontroll, mis kontrollib kas muutujas "täisarv" on arv miinuses rohkem kui -31
             // peale tsüklit kuva sõnum "tsükkel lõppes"
 
+            /*
             int täisArv = 0;
             do
             {
@@ -21,6 +22,8 @@
                 Console.WriteLine(täisArv);
             }while (-31 < täisArv);
             Console.WriteLine("TSÜKKEL LÕPPES!");
+
+            */
 
             // 2. "pikenda sõnumit"
             // tee muutuja "sõnum" mille sees on tühi string, nt ""
@@ -30,6 +33,21 @@
             // kuva muutuja "sõnum" välja
             // -> do while tsükli tingimuseks kirjuta kontroll, mis kontrollib muutuja "sõnum" pikkust .Length abil, kui sõnum on pikem kui 140 tähte, siis tsükkel lõppeb
             // peale tsüklit kuva sõnum "see sõnum enam twitterisse ei mahuks, kirjuta vähem fam"
+
+            string sõnum = string.Empty;      //sama mis "string sõnum = "";"
+            do
+            {
+
+                Console.WriteLine("Palun lisa sõnumi järgmine osa");
+                string sisestus = string.Empty;
+                sisestus = Console.ReadLine();
+                sõnum += sisestus;
+                Console.WriteLine(sõnum);
+
+            } while (sõnum.Length <= 140);
+
+            Console.WriteLine("see sõnum enam twitterisse ei mahuks, kirjuta vähem fam");
+
         }
     }
 }

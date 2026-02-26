97818a0e5b90962de6616cdeb37fc60f2f2e571f
Opilane TTHK <116211837+peskybirb@users.noreply.github.com>
2026-02-26 15:52:53 +0200

    ei tea mingi veider asi on
    tegin endale harjutus 0 et saaks aru kuidas meetodi töötavad ja proovisin kirjutada järgi õpetaja programmi aga asi läks lappe päris kiirelt.

---
+++ MEETODID/0 - proov meetoditega/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace _0___proov_meetoditega
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("baisic ahh calculator");
+            //muutujad
+            int Arv1 = 0;
+            int Arv2 = 0;
+            string valik = "";
+
+            //küsime muutujasse väärtused
+            Console.WriteLine("Sisesta esimene arv");
+            Arv1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sisesta teine arv");
+            Arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("kas soovid liita või lahutada +  -");
+            valik = Console.ReadLine();
+
+            //valikus prindime knsoolile välja
+            if (valik == "+")
+            {
+                int summa = Liitmine(Arv1, Arv2);
+                Console.WriteLine(summa);
+            }
+            else if (valik == "-")
+            {
+                int summa = Lahutamine(Arv1, Arv2);
+                Console.WriteLine(summa);
+            }
+            else
+            {
+                Console.WriteLine("Pole valikus");
+            }
+        }
+
+        public static int Liitmine(int arv1, int arv2)
+        {
+            return arv1 + arv2;
+        }
+        public static int Lahutamine(int arv1, int arv2)
+        {
+            return arv1 - arv2;
+        }
+    }
+}
---
+++ MEETODID/2 - sulgudega kalkulaator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _2___sulgudega_kalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kirjuta koos sisendi kontrolliga ja kasutaja sisesndi normaliseerimisega
+            //programm küsib kasutajalt kolme tehet , Esimene ja kolmas peaksid olema sulgude vahel , kasutaja saab
+            //määrata igale tehtele märgi programm kuvab vastuse vastavalt tehete järjekorrale.
+            
+            //väga kiirelt kirjutatud kood mida proovisin järgi kirjutada aga asi läks kiirelt metsa ja järg kadus niiet
+            //see nata broken + me no understand whats even going on.
+
+
+            //arusaadav
+            Console.WriteLine("Sulgudega kalkulaator");
+            string[] tehteRida = new string[7];
+            Console.WriteLine("(arv tehe arv) tehe (arv tehe arv)");
+            for (int i = 0; i < tehteRida.Length; i++)
+            {
+  
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"Palun sisesta arv. {i + 1} arv");
+                }
+                else
+                {
+                    Console.WriteLine($"Palun sisesta arv. {i + 1} tehe");
+                }
+                tehteRida[i] = Console.ReadLine();
+            }
+
+            //??????
+            double tehe = ÜksTehe([ÜksTehe([tehteRida[0],tehteRida[1],tehteRida[2]]).ToString(),
+
+               tehteRida[3],
+
+               ÜksTehe([tehteRida[4],tehteRida[5],tehteRida[6]]).ToString()]);
+
+        }
+        
+        // ??
+        //mida see meetod tegema ültse peaks 
+        public static double ÜksTehe(string[] tehteRida)
+        {
+  
+            //miks switch kontrollib tehterea teist elementi?
+            switch (tehteRida[1])
+            {
+                //mhmmm
+                case "+":
+                    Liitmine(double.Parse(a), double.Parse(b));
+                    
+                    break;
+                case "-":
+                    Lahutamine(double.Parse(a), double.Parse(b));
+                    break;
+                case "*":
+                    Korrutamine(double.Parse(a), double.Parse(b));
+                    break;
+                case "/":
+                    Korrutamine(double.Parse(a), double.Parse(b));
+                    break;
+
+                default:
+                    return 0;
+                    break;
+            }
+        }
+        
+        // sellest siin all saab arvu veel
+        public static double Liitmine(double arv1, double arv2) 
+        {
+         return arv1 + arv2;
+        }
+        
+        public static double Lahutamine(double arv1, double arv2) 
+        {
+         return arv1 - arv2;
+        }
+
+        public static double Korrutamine(double arv1, double arv2)
+        {
+            return arv1 * arv2;
+        }
+        public static double Jagamine(double arv1, double arv2)
+        {
+            return arv1 / arv2;
+        }
+
+
+}
+}

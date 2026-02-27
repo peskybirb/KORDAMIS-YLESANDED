417acc1af80d0b1a02e34736ff0da78f6b63f80f
Opilane TTHK <116211837+peskybirb@users.noreply.github.com>
2026-02-27 15:55:07 +0200

    4 - ülesanne poolik
    ruudu arvutus asjandus tehtud

---
+++ MEETODID/3-Ilmaprognoos 2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _3_Ilmaprognoos_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //meetoditega programm - Jõgeva - "Ilmaprognoos 2"
+            //
+            // * SISENDITE KÄSITLUS * 
+            //
+            //¤ Käsitle tekstisisendeid nii, et inimese nimi oleks kõik sõnad esisuurtäht, 
+            //¤ kõik muud tekstisisendid standardiseeri kas suureks või väikeseks
+            //¤ arvsisendid try-catchitakse ning catchi puhul seatakse tsükli tingimus nii, et tsükkel jätkuks ja küsimus küsitakse uuesti
+            //
+            // * PROGRAMMI TÖÖ *
+            //
+            //¤ programmis on tuplede või arrayde list kus hoitakse kolme andmet, string kasutajanimi, string pärisnimi, string parool
+            //¤ programmi töö alguses ei oma list mitte ühtegi andmesisestust. kasutajale antakse valik kas sisse logida või registreerida
+            //¤ kui registreerib, siis tehakse uus sisestus ka listi koos kolme andmega mis kasutajalt küsitakse. täisnimi (ees ja perekonnanimi), soovitud kasutajanimi ja parool
+            //see on kui "andmebaas"
+            //¤ NB! ees ja perekonnanimi küsitakse ühe sisestusega, programm ise peab ümbervormindama ta tavakujule (sisendite käsitlus)
+            //¤ NB! parooli *EI TOHI* mingil kujul ringi töödelda.
+            //¤ sisendi kättesaamiseks on oma meetod
+            //¤ sisendi töötlemiseks on erinevad meetodid (arvsisestuse kontroll, esisuurtähe töötlus, ja parooli (ning soovi korral ka kasutajanime) puhul ei kasutata üldse)
+            //¤ kui kasutaja on registreerinud siis kuvatakse endiselt sisselogimis või registreerimisvõimalus
+            //¤ kui kasutaja logib sisse, esitatakse talle tervitus, tema ees ja perekonnanimega, 
+            //¤ kusjuures perekonnanimi "andmebaasis" olevast kuvamise hetkeks eemaldatakse
+            //¤ Programm siis küsib mitme päeva infot ta sisestada tahab. 
+            //¤ Kui tehakse tühi sisestus, siis teavitatakse et on vaja vähemalt 1 päev sisestada ja küsitakse uuesti
+            //¤ tsüklis küsitakse niimitu korda erinevate päevade kohta infot kui kasutaja ütles.
+            //¤ programm omab ka loendit nädalapäevadest
+            //¤ programm küsib milline nädalapäev vastab esimesena sisestatud andmele, kasutaja vastab ühe tähega ETKNRLP
+            //¤ Kasutades eelnevalt tunnis tehtud näiteülesannet kus kuvame välja temperatuuri pulgana, 
+            //¤ tee meetod, mis võtab sisse temperatuuri ning nädalapäeva nime ja tekitab vastava andmepulga, kuhu *SAMA REA LÕPPU* kirjutatakse temperatuur arvväärtusena ja nädalapäeva nimi
+            //¤ Tsükkel kasutab esimest nädalapäeva ja kuvab meetodi abil kõikide sisestatud päevade info.
+            //¤ peale kuvamist küsib kasutajalt kas ta soovib jätkata, ning kui soovib, siis algab programm uuesti andmete sisestusest, mitte sisselogimisest, 
+            //¤ kui ei, kuvatakse uuesti sisselogimine
+            //
+            // 40% = 3
+            // 65% = 4
+            // 80% = 5
+        }
+    }
+}
---
+++ MEETODID/4-Mahukalkulaator/Program.cs
@@ -0,0 +1,112 @@
+﻿using System.Net.Http.Headers;
+
+namespace _4_Mahukalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kirjuta programm mis
+            //küsib kasutajalt kas ta tahab arvutada oma kasti mahtu, või õlivaadi mahtu
+            //sisendite küsimiseks on oma meetod, mis ei lase programmil edasi liikuda, kuni vastus ei ole tühi, ega üks võimalikest valikutest.
+            //selle tegemiseks annate meetodile parameetrina valikud listis kaasa ja .Contains abil saate kontrollida kas ta on olemas või mitte
+            //kui programm teab kumba ta arvutab toimuvad järgmised tegevused
+            //  kasti puhul, küsitakse kas kast on kuubik või risttahukas.
+            //    - kuubiku puhul küsitakse küljepikkus, antakse parameetrina meetodile KuubiRuumala() kaasa,
+            //      ning kuubiruumala tagastab double tüüpi andmena muutujasse tehte tulemuse. valem otsi internetist.
+            //---------------------------         HETKEL SIIN          ---------------------------------------------------------------- 
+            //    - risttahuka puhul lühima ja pikima külje pikkust ning kõrgust. samamoodi arvutab parameetrite abil 
+            //      meetod RisttahukaRuumala() tulemuse double andmena ja tagastab selle muutujasse.
+            //  Tünni puhul, küsitakse kas ta tünn on kaanega või kaaneta.
+            //    - Kui tal on kaas olemas, siis küsi selle paksust, kui ei ole, määra paksuseks ise 0
+            //      Küsi kasutajalt ka tünni põhja läbimõõtu ja kõrgust ning arvuta SilindriRuumala()
+            //      meetodiga kus parameetrid ka kaasas double tulemus mille tagastad muutujasse
+            //      NB! kaane paksuse arvutad kõrgusest maha, sest kaas võtab õlitünni sees mingi ruumala enda poolt ära.
+            //Kuva kasutajale tema ruumala tulemus peaprogrammis, mitte arvutatavates meetodites.
+        
+
+            Console.WriteLine("Kas sa saaovid arvutada Kasti või Tünni ruumala ?");
+            string[] asjandused = new string[] {"Kast","Tünn"};
+            int valikuArv = asjandused.Length;
+
+            for (int i = 0; i < asjandused.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + asjandused[i]);
+            }
+            int kasutajaValik = Valik(new List<int> {1,2}); 
+            string V2 = string.Empty;
+
+
+            if((kasutajaValik-1) == 0)
+            {
+
+                //geniaalne idee 1 static menu if yk yk 
+
+              Console.WriteLine("kas soovite arvutada 1.kuubiku või 2.risttahuka ruumala");
+              V2 = Console.ReadLine();
+              if (V2 == "1")
+              {
+                    Console.WriteLine("Mis on kuubi küljepikkus ?");
+                    double Kuubikülg = double.Parse(Console.ReadLine());
+                    double vastus = KuubiRuumala(Kuubikülg);
+                    Console.WriteLine("vastuseks tuleb "+vastus);
+              }
+              else if (V2 == "2")
+              {
+                    //geniaalne idee 2 list milles on nee d3 asja või array
+
+
+              }
+              else
+              {
+                    Console.WriteLine("ei tea sellist");
+              }
+
+                
+
+
+            }
+            else if ((kasutajaValik-1) == 1)
+            {
+                Console.WriteLine("Kas Tünnil on kaan või mitte ?");
+
+                
+
+
+
+            }
+
+
+        }
+
+
+
+        public static double RistTahukaRuumala()
+        {
+
+            //geniaalne idee 3 arrays asjsad ja võtab seal maha asju vb töötab ei tea hardcode-ish
+            return 0;
+        }
+ 
+        public static double KuubiRuumala(double külg)
+        {
+            double ruumala = Math.Pow(külg, 3);
+            return Math.Round(ruumala, 2);
+        }
+
+
+        public static int Valik(List<int> asjandus)
+        {
+            int valik;
+            do
+            {
+                Console.WriteLine("Sisesta valik: ");
+                valik = int.Parse(Console.ReadLine());
+
+            } while (!asjandus.Contains(valik));
+            return valik;
+        }
+
+
+    }
+}
